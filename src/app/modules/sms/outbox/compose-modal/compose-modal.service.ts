eafa80b4bc1d8477179cd98271b85c2e4b4b380e
Sarl James Sebios <ssarljames@hotmail.com>
2021-04-19 00:30:32 +0800

    progress

--- src/app/modules/sms/outbox/compose-modal/compose-modal.service.ts
+++ src/app/modules/sms/outbox/compose-modal/compose-modal.service.ts
@@ -1,9 +1,8 @@
-import { ComposeSmsParams } from './compose-modal.service';
 import { ComposeModalComponent } from './compose-modal.component';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { Injectable } from '@angular/core';
 
-export interface ComposeSmsParams{
+export interface ComposeSmsParamsInterface{
   receiver: string,
   message: string,
   args?: any
@@ -14,7 +13,7 @@ export class ComposeSmsService {
 
   constructor(private modal: NgbModal) { }
 
-  public compose(param: ComposeSmsParams): Promise<any>{
+  public compose(param: ComposeSmsParamsInterface): Promise<any>{
     const modal: NgbModalRef = this.modal.open( ComposeModalComponent, { size: 'sm', backdrop: 'static'});
     modal.componentInstance.receiver = param.receiver;
     modal.componentInstance.message = param.message;
